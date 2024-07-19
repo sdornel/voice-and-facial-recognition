6f0d4aa47246f102c108372abbd7e9029bd6fd7f
sdornel <kai_dornel@hotmail.com>
2024-07-19 18:17:20 -0400

    make video open

--- next-voice-and-facial-recognition/src/app/FacialRecognition.tsx
+++ next-voice-and-facial-recognition/src/app/FacialRecognition.tsx
@@ -1,11 +1,36 @@
+import { useEffect, useRef } from "react";
 
 export default function FacialRecognition() {
-    return (
-      <div>
-  
-        
-        
+  const videoRef = useRef(null as any);
+
+  useEffect(() => {
+    const getUserMedia = async () => {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        if (videoRef.current) {
+          videoRef.current.srcObject = stream;
+        }
+      } catch (err) {
+        console.error("Error accessing webcam: ", err);
+      }
+    };
+
+    getUserMedia();
+  }, []);
+
+  return (
+    <div>
+      <h1>Facial Recognition Component</h1>
+      <div style={{ position: 'relative' }} className="margin">
+        <video
+          ref={videoRef}
+          id="inputVideo"
+          autoPlay
+          muted
+          style={{ width: '100%', height: 'auto' }}
+        />
+        <canvas id="overlay" />
       </div>
-    );
+    </div>
+  );
 }
-  
